d7a0bbbd391aebda03c79e4d9608557a143e0d14
imTyp0 <95576921+imTyp0@users.noreply.github.com>
2022-01-30 18:22:18 -0500

    Delete pages directory

--- pages/api/bank.js
+++
@@ -1,4 +0,0 @@
-export default async (req, res) =>
-  fetch('https://bank.hackclub.com/hackpenn')
-    .then(r => r.text())
-    .then(t => res.send(t.replace('<head>', '<head><meta charset="UTF-8">')))
